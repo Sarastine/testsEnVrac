42330c1d5ccea5b4e0ce0e4fc29efa07904996f8
PierreB <floods@hotmail.fr>
2019-03-14 15:22:51 +0100

    Récupération finale

--- assets/js/sudoku.js
+++ assets/js/sudoku.js
@@ -1,84 +1,93 @@
 //Remplit la grille pour décoration
 $('#fullGridButton').click(function () {
-	$('#cell11').val(1);
-	$('#cell12').val(2);
-	$('#cell13').val(3);
-	$('#cell14').val(4);
-	$('#cell15').val(5);
-	$('#cell16').val(6);
-	$('#cell17').val(7);
-	$('#cell18').val(8);
-	$('#cell19').val(9);
-	$('#cell21').val(4);
-	$('#cell22').val(5);
-	$('#cell23').val(6);
-	$('#cell24').val(7);
-	$('#cell25').val(8);
-	$('#cell26').val(9);
-	$('#cell27').val(1);
-	$('#cell28').val(2);
-	$('#cell29').val(3);
-	$('#cell31').val(7);
-	$('#cell32').val(8);
-	$('#cell33').val(9);
-	$('#cell34').val(1);
-	$('#cell35').val(2);
-	$('#cell36').val(3);
-	$('#cell37').val(4);
-	$('#cell38').val(5);
-	$('#cell39').val(6);
-	$('#cell41').val(2);
-	$('#cell42').val(3);
-	$('#cell43').val(1);
-	$('#cell44').val(5);
-	$('#cell45').val(6);
-	$('#cell46').val(4);
-	$('#cell47').val(8);
-	$('#cell48').val(9);
-	$('#cell49').val(7);
-	$('#cell51').val(5);
-	$('#cell52').val(6);
-	$('#cell53').val(4);
-	$('#cell54').val(8);
-	$('#cell55').val(9);
-	$('#cell56').val(7);
-	$('#cell57').val(2);
-	$('#cell58').val(3);
-	$('#cell59').val(1);
-	$('#cell61').val(8);
-	$('#cell62').val(9);
-	$('#cell63').val(7);
-	$('#cell64').val(2);
-	$('#cell65').val(3);
-	$('#cell66').val(1);
-	$('#cell67').val(5);
-	$('#cell68').val(6);
-	$('#cell69').val(4);
-	$('#cell71').val(3);
-	$('#cell72').val(1);
-	$('#cell73').val(2);
-	$('#cell74').val(6);
-	$('#cell75').val(4);
-	$('#cell76').val(5);
-	$('#cell77').val(9);
-	$('#cell78').val(7);
-	$('#cell79').val(8);
-	$('#cell81').val(6);
-	$('#cell82').val(4);
-	$('#cell83').val(5);
-	$('#cell84').val(9);
-	$('#cell85').val(7);
-	$('#cell86').val(8);
-	$('#cell87').val(3);
-	$('#cell88').val(1);
-	$('#cell89').val(2);
-	$('#cell91').val(9);
-	$('#cell92').val(7);
-	$('#cell93').val(8);
-	$('#cell94').val(3);
-	$('#cell95').val(1);
-	$('#cell96').val(2);
-	$('#cell97').val(6);
-	$('#cell98').val(4);
-	$('#cell99').val(5);
+	$('#cell11,#cell27,#cell34,#cell43,#cell59,#cell66,#cell72,#cell88,#cell95').val(1);
+	$('#cell12,#cell28,#cell35,#cell41,#cell57,#cell64,#cell73,#cell89,#cell96').val(2);
+	$('#cell13,#cell29,#cell36,#cell42,#cell58,#cell65,#cell71,#cell87,#cell94').val(3);
+	$('#cell14,#cell21,#cell37,#cell46,#cell53,#cell69,#cell75,#cell82,#cell98').val(4);
+	$('#cell15,#cell22,#cell38,#cell44,#cell51,#cell67,#cell76,#cell83,#cell99').val(5);
+	$('#cell16,#cell23,#cell39,#cell45,#cell52,#cell68,#cell74,#cell81,#cell97').val(6);
+	$('#cell17,#cell24,#cell31,#cell49,#cell56,#cell63,#cell78,#cell85,#cell92').val(7);
+	$('#cell18,#cell25,#cell32,#cell47,#cell54,#cell61,#cell79,#cell86,#cell93').val(8);
+	$('#cell19,#cell26,#cell33,#cell48,#cell55,#cell62,#cell77,#cell84,#cell91').val(9);
+});
+
+//Teste une cellule et la remplit si un seul chiffre possible dedans
+function testCell(){
+	/*Prendre une cellule.
+	Si elle est vide :
+		Dans sa ligne, vérifier s'il n'y a pas de 1.
+		Si pas de 1, jeton 1.
+		Idem pour 2, 3, 4, 5, 6, 7, 8 et 9.
+		Dans sa colonne, vérifier s'il n'y a pas de 1.
+		Si pas de 1, jeton 1.
+		Idem pour 2, 3, 4, 5, 6, 7, 8 et 9.
+		Dans son carré, vérifier s'il n'y a pas de 1.
+		Si pas de 1, jeton 1.
+		Idem pour 2, 3, 4, 5, 6, 7, 8 et 9.
+		Si un et un seul jeton, mettre le chiffre dans la cellule*/
+	var tokenOne = tokenTwo = tokenThree = tokenFour = tokenFive = tokenSix = tokenSeven = tokenEight = tokenNine = true;
+	if ($('#cell11').val() == "") {
+		$('.lineOne').each(function(){
+			console.log($(this).val());
+			switch ($(this).val()) {
+				case "1":
+					tokenOne = false;
+					break;
+				case "2":
+					tokenTwo = false;
+					break;
+				case "3":
+					tokenThree = false;
+					break;
+				case "4":
+					tokenFour = false;
+					break;
+				case "5":
+					tokenFive = false;
+					break;
+				case "6":
+					tokenSix = false;
+					break;
+				case "7":
+					tokenSeven = false;
+					break;
+				case "8":
+					tokenEight = false;
+					break;
+				case "9":
+					tokenNine = false;
+					break;
+				default:
+					console.log("Cellule vide");
+			}
+		});
+		console.log("Les jetons sont à " + tokenOne + tokenTwo + tokenThree + tokenFour + tokenFive + tokenSix + tokenSeven + tokenEight + tokenNine);
+		if (tokenOne && !(tokenTwo || tokenThree || tokenFour || tokenFive || tokenSix || tokenSeven || tokenEight || tokenNine)) {
+			$('#cell11').val(1);
+		} else if (tokenTwo && !(tokenOne || tokenThree || tokenFour || tokenFive || tokenSix || tokenSeven || tokenEight || tokenNine)) {
+			$('#cell11').val(2);
+		} else if (tokenThree && !(tokenOne || tokenTwo || tokenFour || tokenFive || tokenSix || tokenSeven || tokenEight || tokenNine)) {
+			$('#cell11').val(3);
+		} else if (tokenFour && !(tokenOne || tokenThree || tokenTwo || tokenFive || tokenSix || tokenSeven || tokenEight || tokenNine)) {
+			$('#cell11').val(4);
+		} else if (tokenFive && !(tokenOne || tokenThree || tokenFour || tokenTwo || tokenSix || tokenSeven || tokenEight || tokenNine)) {
+			$('#cell11').val(5);
+		} else if (tokenSix && !(tokenOne || tokenThree || tokenFour || tokenFive || tokenTwo || tokenSeven || tokenEight || tokenNine)) {
+			$('#cell11').val(6);
+		} else if (tokenSeven && !(tokenOne || tokenThree || tokenFour || tokenFive || tokenSix || tokenTwo || tokenEight || tokenNine)) {
+			$('#cell11').val(7);
+		} else if (tokenEight && !(tokenOne || tokenThree || tokenFour || tokenFive || tokenSix || tokenSeven || tokenTwo || tokenNine)) {
+			$('#cell11').val(8);
+		} else if (tokenNine && !(tokenOne || tokenThree || tokenFour || tokenFive || tokenSix || tokenSeven || tokenEight || tokenTwo)) {
+			$('#cell11').val(9);
+		} else {
+			console.log("Plus d'une possibilité");
+		}
+	}
+}
+
+//Test, lance la fonction en cours de développement
+$('#TEST').click(function(){
+	$(this).removeClass('btn-warning').addClass('btn-light');
+	testCell();
 });
